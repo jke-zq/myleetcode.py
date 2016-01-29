5ec26905ca5b256cbecdcef6babb48a3bda892c0
jke0zq <jke0zq@gmail.com>
2016-01-29 23:14:40 +0800

    Factorial Trailing Zeroes

---
+++ Factorial Trailing Zeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while n:
+            count += n / 5
+            n /= 5
+        return count
