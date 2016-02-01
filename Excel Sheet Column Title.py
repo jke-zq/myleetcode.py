713760fd11be31a221160743e2737ac120a1ac3e
jke0zq <jke0zq@gmail.com>
2016-02-02 00:19:19 +0800

    Excel Sheet Column Title

---
+++ Excel Sheet Column Title.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        # ret = ""
+        # while n:
+        #     left = n % 26
+        #     if left == 0:
+        #         # n -= 1
+        #         left = 26
+        #     ret += chr(ord("A") + left - 1)
+        #     n = (n - 1) / 26
+        # return ret[::-1]
+        ##solution B
+        result, dvd = "", n
+        
+        while dvd:
+            dvd -= 1
+            result += chr(dvd % 26 + ord('A'))
+            dvd /= 26
+        return result[::-1]
