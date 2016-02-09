f30758cec09765e20f310b8d5fcc6584188e723d
jke0zq <jke0zq@gmail.com>
2016-02-09 22:29:12 +0800

    Majority Element II

---
+++ Majority Element II.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        k = 2
+        dicts = collections.defaultdict(int)
+        for n in nums:
+            dicts[n] += 1
+            while len(dicts) > k:
+                for x in dicts.keys():
+                    dicts[x] -= 1
+                    if dicts[x] == 0:
+                        dicts.pop(x)
+        dicts = dicts.fromkeys(dicts, 0)
+        for n in nums:
+            if n in dicts:
+                dicts[n] += 1
+        return filter(lambda x:dicts[x] > float(len(nums) / 3), dicts.keys())
