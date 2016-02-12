f422f9370e589378bf135e94b225106ac9d088c9
jke0zq <jke0zq@gmail.com>
2016-02-12 22:25:51 +0800

    Sort Colors

---
+++ Sort Colors.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        size = len(nums)
+        left, right = 0, size - 1
+        i = 0
+        while i <= right:
+            if nums[i] == 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
+                i += 1
+                ##because of there is not '2 between left and i.
+                # if i == left - 1:
+                #     i += 1
+            elif nums[i] == 2:
+                nums[right], nums[i] = nums[i], nums[right]
+                right -= 1
+            else:
+                i += 1
+        
+            
