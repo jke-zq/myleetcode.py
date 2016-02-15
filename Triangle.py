382b250123bbe940dc7355970e325b07334995f9
jke0zq <jke0zq@gmail.com>
2016-02-15 21:38:16 +0800

    Triangle

--- Triangle.py
+++ Triangle.py
@@ -7,12 +7,20 @@ class Solution(object):
         ret = [0] * (len(triangle[-1]) + 1)
         for t in triangle:
             size = len(t)
-            ret[0] = ret[1]
-            for i in range(size, 0, -1):
-                if i == size:
-                    ret[i] = ret[i - 1] + t[i - 1]
-                else:
-                    ret[i] = min(ret[i], ret[i - 1]) + t[i - 1]
+
+            i = size
+            ret[i] = ret[i - 1] + t[i - 1]
+            for i in range(size - 1, 1, -1):
+                ret[i] = min(ret[i], ret[i - 1]) + t[i - 1]
+            if size > 1:
+                i = 1
+                ret[i] = ret[i] + t[i - 1]
+            # ret[0] = ret[1]
+            # for i in range(size, 0, -1):
+            #     if i == size:
+            #         ret[i] = ret[i - 1] + t[i - 1]
+            #     else:
+            #         ret[i] = min(ret[i], ret[i - 1]) + t[i - 1]
         
         return min(ret[1:])
                     
