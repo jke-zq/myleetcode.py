aac2081b384610cf48088f79235245d179ac8bb5
jke0zq <jke0zq@gmail.com>
2016-02-24 00:00:41 +0800

    odify

--- Lowest Common Ancestor of a Binary Tree.py
+++ Lowest Common Ancestor of a Binary Tree.py
@@ -13,21 +13,30 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
-        def findPath(tmp, node, target):
-            if not node:
-                return
-            else:
-                tmp.append(node)
-                if node == target:
-                    return True
-                if not findPath(tmp, node.left, target) and not findPath(tmp, node.right, target):
-                    tmp.pop()
-                    return False
-                return True
-        ppath, qpath = [], []
-        findPath(ppath, root, p)
-        findPath(qpath, root, q)
-        for n in qpath[::-1]:
-            if n in ppath:
-                return n
-        return None
+        # def findPath(tmp, node, target):
+        #     if not node:
+        #         return
+        #     else:
+        #         tmp.append(node)
+        #         if node == target:
+        #             return True
+        #         if not findPath(tmp, node.left, target) and not findPath(tmp, node.right, target):
+        #             tmp.pop()
+        #             return False
+        #         return True
+        # ppath, qpath = [], []
+        # findPath(ppath, root, p)
+        # findPath(qpath, root, q)
+        # for n in qpath[::-1]:
+        #     if n in ppath:
+        #         return n
+        # return None
+        ##note:the p and q node in the tree
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+        ##Runtime Error
+        # left, right = map(lambda x:self.lowestCommonAncestor(x, p, q), [root.left, root.right])
+        left, right = [self.lowestCommonAncestor(child, p, q) for child in (root.left, root.right)]
+        return root if left and right else (left or right)
