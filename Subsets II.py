b5d3802d2720935b785820c7f91bd35bff3e528f
jke0zq <jke0zq@gmail.com>
2016-05-17 17:22:18 +0800

    dfs

--- Subsets II.py
+++ Subsets II.py
@@ -1,21 +1,32 @@
 class Solution(object):
-    def subsetsWithDup(self, nums):
+    def permuteUnique(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        def dfs(start, end, nums, tmp, ret):
-            ret.append(tmp[::])
-            for i in range(start, end):
-                if i > start and nums[i] == nums[i - 1]:
+        def dfs(start, length, visited, tmp, ans, nums):
+            if start == length:
+                ans.append(tmp[:])
+                return
+            for i in range(length):
+                if i > 0 and nums[i] == nums[i - 1] and not visited[i - 1]:
                     continue
-                else:
-                    tmp.append(nums[i])
-                    dfs(i + 1, end, nums, tmp, ret)
-                    tmp.pop()
-                    
-        nums.sort()
+                ## error
+                if visited[i]:
+                    continue
+                tmp.append(nums[i])
+                visited[i] = True
+                dfs(start + 1, length, visited, tmp, ans, nums)
+                visited[i] = False
+                tmp.pop()
+                
+        if not nums:
+            return []
+        
+        nums = sorted(nums)
         length = len(nums)
-        ret, tmp = [], []
-        dfs(0, length, nums, tmp, ret)
-        return ret
+        tmp, ans = [], []
+        visited = [False] * length
+        dfs(0, length, visited, tmp, ans, nums)
+        return ans
+        
