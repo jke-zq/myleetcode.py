d58a5b1d94bd94cf690d97690827c5987c505280
jke0zq <jke0zq@gmail.com>
2016-07-28 22:48:29 +0800

    Kth Smallest Element in a BST.py

---
+++ Kth Smallest Element in a BST.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        cur = root
+        while k:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            if k == 1:
+                return cur.val
+            k -= 1
+            cur = cur.right
+        
+#########################################           
+#### need to be done:
+#### to simplify the codes.  
